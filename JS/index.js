cfa34c8cd44cba7ccefe42dc4f225ed4c2984ba8
unknown <makhalbiswajit1@gmail.com>
2025-05-30 18:15:47 +0530

    index restaurant

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+  $("#button").click(function(){
+    $("#list").slideToggle("slow");
+  });
+});
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
