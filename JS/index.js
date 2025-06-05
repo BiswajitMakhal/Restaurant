8428b67ef0f130c8085cedb2d296901604ad76c0
unknown <makhalbiswajit1@gmail.com>
2025-06-05 23:20:20 +0530

    bootstrap

--- JS/index.js
+++ JS/index.js
@@ -1,24 +1,20 @@
-
-$(document).ready(function(){
-  $("#button").click(function(){
-    $("#list").slideToggle("slow");
-  });
-});
+// FOR AOS
+AOS.init();
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
+  slidesPerView: 3.5,
+  spaceBetween: 20,
   loop: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    clickable:true,
+     clickable: true,
   },
+  
+  
+  
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
 });
